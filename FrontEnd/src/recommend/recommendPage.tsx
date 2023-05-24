eb4f36a7bbccb25ecdc2b20d8d5f6938791cee37
육종호 <yookjongho@yugjonghoui-MacBookAir.local>
2023-05-24 21:05:58 +0900

    add library spinner components

--- FrontEnd/src/recommend/recommendPage.tsx
+++ FrontEnd/src/recommend/recommendPage.tsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Button from "../components/button";
+import Select from "./select";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 2rem;
+  border: 1px solid black;
+  border-radius: 1%;
+  width: 80%;
+  margin: 1rem auto;
 
   span {
     font-family: "NanumSquareRound";
     font-style: normal;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
   }
 
   .tabs {
-    margin: 1rem;
+    margin: 1rem 0 3rem 0;
     padding: 0px;
     list-style: none;
     color: #black;
@@ -40,29 +47,29 @@ function Recommend() {
   //   const [isOpen, setIsOpen] = useState(false);
   const seasons = ["SPRING", "SUMMER", "AUTUMN", "WINTER"];
   const color = ["ffc5dc", "8FCEFF", "bc944c", "9bb6b9"];
-  const categories = ["TOP", "BOTTOM", "OUTER", "SHOES"];
+  const categories = ["TOP", "BOTTOM", "OUTER", "SHOES", "ACCESSORY"];
 
   const [season, setSeason] = useState(seasons[0]);
   const [currentSeason, setCurrentSeason] = useState(0);
 
-  // const [category, setCategory] = useState(categories[0]);
-  // const [currentCategory, setCurrentCategory] = useState(0);
+  const [category, setCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(0);
 
   const SeasonClick = (idx: number) => {
     setSeason(seasons[idx]);
     setCurrentSeason(idx);
   };
 
-  // const handleCategory = (idx: number) => {
-  //   setCategory(categories[idx]);
-  //   setCurrentCategory(idx);
-  // };
+  const handleCategory = (idx: number) => {
+    setCategory(categories[idx]);
+    setCurrentCategory(idx);
+  };
 
   return (
     <>
       <Container>
         <span>
-          <b>계절을 선택해주세요.</b>
+          <b>"계절"을 선택해주세요</b>
         </span>
         <ul className="tabs">
           {seasons.map((element, index) => {
@@ -85,13 +92,11 @@ function Recommend() {
             );
           })}
         </ul>
-        <span style={{ textAlign: "center" }}>
-          <b>
-            원하는 옷을 골라주세요! <br></br>
-            에코가 그에 맞는 코디를 추천해드려요
-          </b>
+        <span>
+          <b>"함께" 추천받을 옷을 골라주세요</b>
         </span>
-        {/* <ul className="tabs">
+
+        <ul className="tabs">
           {categories.map((element, index) => {
             return (
               <li
@@ -111,7 +116,7 @@ function Recommend() {
             );
           })}
         </ul>
-        <Select category={category}></Select> */}
+        <Select category={category} season={season}></Select>
       </Container>
     </>
   );
