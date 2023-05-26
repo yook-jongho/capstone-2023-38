a1bfa7452bc2b804758e7231d476d6fedbd1a09a
육종호 <yookjongho@yugjonghoui-MacBookAir.local>
2023-05-26 09:23:18 +0900

    design change

--- FrontEnd/src/feedPage/feedPage.tsx
+++ FrontEnd/src/feedPage/feedPage.tsx
@@ -5,54 +5,89 @@ import styled from "styled-components";
 import MainBox from "../components/mainBox";
 import useAsync, { getFeedPage, getTest } from "../customhook/useAsync";
 
+const SortStandard = styled.div`
+  margin: 2rem;
+  span {
+    marign: 0.4rem;
+    padding: 1rem;
+    cursor: pointer;
+
+    transition: font-size 0.3s ease-in-out;
+
+    &:hover {
+      font-size: 1.2rem;
+    }
+  }
+`;
+
 const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-
   width: 100%;
   justify-content: center;
 `;
-const CodyContainer = styled.div`
-  text-align: center;
 
+const CodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 2rem;
-  padding: 2rem;
 
   .img {
     max-width: 300px;
     max-height: 300px;
+    border: 5px solid #white;
     border-radius: 0.2rem;
-    // box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
   }
 
-  div > span {
-    margin: 1rem;
+  .id_upload {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .id_like {
+    display: flex;
+    flex-direction: column;
   }
 `;
+
 function FeedPage() {
-  // const [state, fetchData] = useAsync(() => getFeedPage(), true);
   const [state, fetchData] = useAsync(() => getTest("codyfeed"), true);
   const { loading, data, error } = state;
+
+  const getUploadTime = (uploadTime: Date) => {
+    const parsedDate = new Date(uploadTime);
+    const time = parsedDate.toLocaleTimeString();
+
+    return time;
+  };
 
   const renderFeedList = () => {
     if (loading) return <div>loading...</div>;
     if (error) return <div>error message {error.message}</div>;
     if (!data) return null;
-    // if (data) console.log(data);
-    console.log(data);
 
     return (
       <>
-        {data.map((data: any) => (
-          <CodyContainer key={data.userid}>
-            <img className="img" src={data.imgsrc}></img>
-            <div>
-              <span>{data.userid}</span>
-              <span>
-                <FontAwesomeIcon icon={faHeart} style={{ color: "#ff0000" }} />{" "}
-                {data.wcount}
-              </span>
+        {data.map((item: any) => (
+          <CodyContainer key={item.imgsrc}>
+            <img className="img" src={item.imgsrc} alt="Cody Image" />
+            <div className="id_upload">
+              <div className="id_like">
+                <span style={{ marginTop: "10px" }}>{item.userid}</span>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    style={{ color: "#ff0000" }}
+                  />{" "}
+                  {item.wcount}
+                </span>
+              </div>
+              <span>{getUploadTime(item.uploadtime)}</span>
             </div>
           </CodyContainer>
         ))}
@@ -61,10 +96,14 @@ function FeedPage() {
   };
 
   return (
-    <MainContainer>
-      {/* {renderFeedList()} */}
-      {renderFeedList()}
-    </MainContainer>
+    <>
+      <SortStandard>
+        <span>인기순 </span>
+        <span> | </span>
+        <span>최신순 </span>
+      </SortStandard>
+      <MainContainer>{renderFeedList()}</MainContainer>
+    </>
   );
 }
 
